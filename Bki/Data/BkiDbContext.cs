b834fa11812f9f6d225bbe337b95335015136d31
Alexey Rassolov <alexey.rassolov@gmail.com>
2021-05-05 20:58:55 +0300

    bki service w/ ui added, contains page to add loan and put it in db

---
+++ Bki/Data/BkiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bki.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bki.Data
+{
+    public class BkiDbContext : DbContext
+    {
+        public BkiDbContext(DbContextOptions<BkiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<LoanRequest> LoanRequests { get; set; }
+    }
+}
---
+++ Bki/Models/LoanRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bki.Models
+{
+    [Index(nameof(Passport))]
+    public class LoanRequest
+    {
+        public LoanRequest(string passport, int amount)
+        {
+            Passport = passport;
+            Amount = amount;
+        }
+
+        public int Id { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Passport { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
