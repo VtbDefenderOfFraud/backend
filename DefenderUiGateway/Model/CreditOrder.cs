531940782b6f27991ad8959e2a5f361f3cc55867
Grom0zeka <artem.pavelyev@yandex.ru>
2021-05-05 19:59:50 +0300

    Stubs for credit controllers were added

---
+++ DefenderUiGateway/Model/CreditOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefenderUiGateway.Model
+{
+    public class CreditOrder
+    {
+        public CreditOrder(string bankName, string registrationNumber, string tin, DateTime orderDate)
+        {
+            BankName = bankName;
+            RegistrationNumber = registrationNumber;
+            Tin = tin;
+            OrderDate = orderDate;
+        }
+
+        public string BankName { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Tin { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
